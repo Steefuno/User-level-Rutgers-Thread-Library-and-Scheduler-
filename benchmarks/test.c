d5323bcba2abaea263ed8bab10cf68dfcf7a22dd
Ethan E. Febinger <eef45@popsicle.cs.rutgers.edu>
2020-03-10 04:37:11 -0400

    added MLFQ scheduler, but still need to implement priority boosting for it

--- benchmarks/test.c
+++ benchmarks/test.c
@@ -4,17 +4,17 @@
 #include "../rpthread.h"
 
 int cc = 0;
-pthread_mutex_t* mutex1;
+//pthread_mutex_t* mutex1;
 
 void* count(void* argv) {
 	int i = 0;
 
-	pthread_mutex_lock(mutex1);
+	//pthread_mutex_lock(mutex1);
 	while (i < (int)argv) {
 		cc++;
 		i++;
 	}
-	pthread_mutex_unlock(mutex1);
+	//pthread_mutex_unlock(mutex1);
 
 	printf(">Exiting\n");
 	pthread_exit(NULL);
@@ -22,8 +22,8 @@ void* count(void* argv) {
 
 int main(int argc, char **argv) {
 	pthread_t thread1, thread2;
-	printf(">Init Mutex\n");
-	pthread_mutex_init(mutex1, NULL);
+	//printf(">Init Mutex\n");
+	//pthread_mutex_init(mutex1, NULL);
 
 	printf(">Creating1\n");
 	pthread_create(&thread1, NULL, count, (void*)1000000);
